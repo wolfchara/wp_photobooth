368f78df6597e5a153a92fe8cd64e3abf6fd2af6
Wolfchara <vovikys1997@gmail.com>
2020-01-29 18:54:17 +0200

    commit some rows

--- footer.php
+++ footer.php
@@ -35,7 +35,7 @@
       </a>
     </div>
   </footer>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
   <!-- <script src="assets/js/main.js"></script> -->
 
   <?php wp_footer(); ?>
--- header.php
+++ header.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <link rel="shortcut icon" type="image/x-icon" href="http://photobooth/wp-content/themes/photobooth/assets/img/icons/favicon.png" />
-  <link href="https://fonts.googleapis.com/css?family=Aleo:400,700|Josefin+Sans:300,400,600,700&display=swap&" rel="stylesheet" />
+  <!-- <link rel="shortcut icon" type="image/x-icon" href="http://photobooth/wp-content/themes/photobooth/assets/img/icons/favicon.png" /> -->
+  <!-- <link href="https://fonts.googleapis.com/css?family=Aleo:400,700|Josefin+Sans:300,400,600,700&display=swap&" rel="stylesheet" /> -->
   <!-- <link rel="stylesheet" href="http://photobooth/wp-content/themes/photobooth/assets/css/normalize.css" />
   <link rel="stylesheet" href="http://photobooth/wp-content/themes/photobooth/style.css" /> -->
   <!-- <title>Photobooth</title> -->
